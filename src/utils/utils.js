9e0ab9b1816b9f7adc46c33a56539aa7f9e69e42
hramov <trykhramov@gmail.com>
2022-02-21 13:23:37 +0700

    Temporarely remove hashing password

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,11 @@
-import { genSalt, hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export async function hashPassword(password) {
-    const salt = await genSalt(10);
+    const salt = parseInt('salt');
     const hashedPassword = await hash(password, salt);
     return hashedPassword;
+}
+
+export async function isUser(password, hashedPassword) {
+    return await compare(password, hashedPassword)
 }
